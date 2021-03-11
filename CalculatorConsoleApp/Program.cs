59cc5deba71ab47ec40a7f35843465d93972a303
pacar nya Im Nayeon <izunaka8@gmail.com>
2021-03-11 13:07:44 +0700

    halo

---
+++ CalculatorConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CalculatorConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Test Calculator";
+            Console.Write("Bilangan pertama: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Bilangan kedua: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hasil Penambahan " + a + " + " + b + " = " + Penambahan(a, b));
+            Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
+            Console.WriteLine("Hasil Perkalian {0} * {1} = {2}", a, b, Perkalian(a, b));
+            Console.WriteLine("Hasil Pembagian {0} / {1} = {2}", a, b, Pembagian(a, b));
+            Console.ReadKey();
+        
+    }
+
+        static int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+
+        static int Pembagian(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
